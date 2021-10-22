ab5e10c78e4b9fefdd586bce9db7d76dbf2d2a17
Anton Ustin <ustin.a@tecomgroup.ru>
2021-10-22 10:40:48 +0300

    update

--- run-tui.sh
+++ run-tui.sh
@@ -1,1 +1,12 @@
- 
+#!/bin/bash
+case $1 in
+    "--dialog")
+        dialog --yesno "Everything works?" 5 30
+    ;;
+    "--whiptail")
+        whiptail --yesno "Everything works?" 9 30
+    ;;
+    *)
+        echo "Run with arg --whiptail or --dialog"
+    ;;
+esac
